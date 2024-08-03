28f6573741122bd66c1267f7a818d54c21b3fe81
Farin Jakate <150505333+farinjakate@users.noreply.github.com>
2024-08-03 15:59:53 +0530

    Add files via upload

---
+++ ProductBL.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class ProductBL : IProductBL
+    {
+        public bool CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
