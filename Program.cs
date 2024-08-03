8b5197684aac40b16a920a74102e8399d7106dd8
Farin Jakate <150505333+farinjakate@users.noreply.github.com>
2024-08-03 15:54:18 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+//using Ado.net in Dot Net Core Application
 
-namespace ADONETCoreApp
+// See https://aka.ms/new-console-template for more information
+
+using ADONetCoreAppp;
+using Microsoft.Data.SqlClient;
+using System.Numerics;
+
+Console.WriteLine("***All Trainers***");
+
+
+
+/////    comment kiye huye code ko copy karke  AllTrainers() method me paste kiya hai       ////
+
+
+//List<Trainer> trainers = new List<Trainer>();//Trainer ki List Banaye
+
+//string connectionString = "server=.\\sqlexpress;database=MYADONETDB;integrated security=true;encrypt=false;";
+////so here we have to do some changes in .net core encrypt=false;
+
+//SqlConnection con = new SqlConnection(connectionString);
+
+//SqlCommand cmd = new SqlCommand("select * from Trainer", con); //select * from Trainer ye ek  inline(adoc)query hai
+//con.Open();
+//SqlDataReader reader = cmd.ExecuteReader();
+
+//if (reader.HasRows) // HasRows check krta hai Rows hai kya
+//{
+//    while (reader.Read())
+//    {
+//        Trainer t = new Trainer() //object Initializer syntax
+//        {
+//            Id = (int)reader["Id"],
+//            Name = reader["Name"].ToString(),
+//            City = reader["City"].ToString(),
+//            Experience = (int)reader["Experience"]
+//        };
+//        trainers.Add(t);//Trainer List yaha Pr Add Kiye
+//    }
+//}
+//else
+//{
+//    trainers = null;
+//}
+
+//con.Close();
+
+
+
+
+
+
+
+
+
+TrainerDB db = new TrainerDB();
+var trainers = db.AllTrainers();
+
+foreach (var item in trainers)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-
-
-        }
-    }
+    Console.WriteLine($"Id:{item.Id} Name:{item.Name}  city:{item.City} Experience:{item.Experience}");
 }
+
+Console.WriteLine("Create A New Trainer");
+Console.WriteLine("Please enter name");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter city");
+string city = Console.ReadLine();
+Console.WriteLine("Please enter experience");
+int experience =int.Parse(Console.ReadLine());
+
+Trainer t = new Trainer() { Name = name, City = city, Experience = experience };
+
+int rn;
+if(db.CreateTrainer(t,out rn))
+{
+    Console.WriteLine($"Trainer Inserted successfully.Trainer Id is {rn}");
+
+}
+else
+{
+    Console.WriteLine("Failed to insert new trainer");
+
+}
+
+
+Console.WriteLine("\nEnter trainer id to delete");
+int id = int.Parse(Console.ReadLine());
+
+bool status = db.DeleteTrainer(id);
+if(status)
+{
+    Console.WriteLine($"Trainer with id {id} Deleted successfully,");
+}
+else
+{
+    Console.WriteLine($"Trainer with Id {id} Delete failed.");
+}
+
+
+
+
+Console.ReadLine();
+Console.WriteLine("Hello, World!");
+
+
+
---
+++ TrainerDB.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONetCoreAppp
+{
+    // ye dot net core Application ka code hai isme srif ek hi change hai encrypted=false;
+    public class TrainerDB
+    {
+        public List<Trainer> AllTrainers()//Trainer table ka pura data copy kiye hai.
+        {
+            List<Trainer> trainers = new List<Trainer>();//Trainer ki List Banaye
+
+            string connectionString = "server=.\\sqlexpress;database=MYADONETDB;integrated security=true;encrypt=false;";
+            //so here we have to do some changes in .net core encrypt=false;
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("select * from Trainer", con); //select * from Trainer ye ek  inline(adoc)query hai
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows) // HasRows check krta hai Rows hai kya
+            {
+                while (reader.Read())
+                {
+                    Trainer t = new Trainer() //object Initializer syntax
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        City = reader["City"].ToString(),
+                        Experience = (int)reader["Experience"]
+                    };
+                    trainers.Add(t);//Trainer List yaha Pr Add Kiye
+                }
+            }
+            else
+            {
+                trainers = null;
+            }
+            con.Close();
+
+            return trainers;
+        }
+
+        public bool DeleteTrainer(int id)
+        {
+            string connextionString =
+                    "server=.\\sqlexpress;database=MYADONETDB;integrated security=true;encrypt=false;";
+
+            //string connextionString =
+            //  ConfigurationManager.ConnectionStrings["ADONETDB"].ConnectionString;
+
+            // [] bracket in connectionstring name which is written in App.config file.
+
+            SqlConnection con = new SqlConnection(connextionString);
+
+            SqlCommand cmd = new SqlCommand("uspDeleteTrainer", con);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Id", id);
+
+
+            SqlParameter status = new SqlParameter()
+            {
+                ParameterName = "@Status",
+                SqlDbType = SqlDbType.Bit,
+                Direction = ParameterDirection.Output,
+            };
+
+            cmd.Parameters.Add(status);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();//ExecuteNonQuery command class hai, use for prepared & execute command
+
+            con.Close();
+
+            return (bool)status.Value;
+        }
+
+        public bool CreateTrainer(Trainer trainer, out int rollNumber)
+        {
+            string connectionString =
+                "server=.\\sqlexpress;database=MYADONETDB;integrated security=true;encrypt=false;";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("uspCreateTrainer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", trainer.Name);
+            cmd.Parameters.AddWithValue("@City", trainer.City);
+            cmd.Parameters.AddWithValue("@Experience", trainer.Experience);
+
+            SqlParameter rn = new SqlParameter()
+            {
+                ParameterName = "@RollNumber",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Output
+            };
+            cmd.Parameters.Add(rn);
+
+            SqlParameter status = new SqlParameter()
+            {
+                ParameterName = "@Status",
+                SqlDbType = SqlDbType.Bit,
+                Direction = ParameterDirection.Output
+            };
+            cmd.Parameters.Add(status);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            rollNumber = (int)rn.Value;
+            con.Close();
+            return (bool)status.Value;
+           
+        }
+
+       
+
+    }
+}
